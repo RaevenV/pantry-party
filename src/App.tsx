5ef2bda47fa8580bfa088fb645c67735687990b5
RaevenV <villarelr@gmail.com>
2024-10-02 20:31:11 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="w-full bg-slate-400 min-h-screen">test</div>
+    </>
+  );
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Add TypeScript and JSX files
+  theme: {
+    extend: {
+      // You can add custom colors, spacing, etc., here
+    },
+  },
+  plugins: [
+    // Add Tailwind plugins here if needed, like typography, forms, etc.
+  ],
+};
