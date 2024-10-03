19a4adf1dd1f46dc6dd67e359e3821e6353486e5
RaevenV <villarelr@gmail.com>
2024-10-03 12:27:09 +0700

    v3

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
